f5e1a3248e8ea59f19fce9e6fc029f59e51e168d
Subhankar Saha <subhankarsaha2410@gmail.com>
2024-02-12 01:57:23 +0530

    add home

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -9,4 +9,36 @@ export class HomeComponent {
   constructor() {
     console.log("welcome")
   }
+  public imageBannerArr: any = [
+		{
+			name: "Image1",
+			description: "AC Haua",
+			src : "../../../assets/banner/image2fan.jpg",
+		},
+		{
+			name: "Image1",
+			description: "AC Haua",
+			src : "../../../assets/banner/image1fan.png",
+		},
+    {
+			name: "Image1",
+			description: "AC Haua",
+			src : "../../../assets/banner/image2fan.jpg",
+		},
+		{
+			name: "Image1",
+			description: "AC Haua",
+			src : "../../../assets/banner/image1fan.png",
+		},
+    {
+			name: "Image1",
+			description: "AC Haua",
+			src : "../../../assets/banner/image2fan.jpg",
+		},
+		{
+			name: "Image1",
+			description: "AC Haua",
+			src : "../../../assets/banner/image1fan.png"
+		},
+	]
 }
--- src/app/navigation/topbar/topbar.component.ts
+++ src/app/navigation/topbar/topbar.component.ts
@@ -13,42 +13,42 @@ import { SnackService } from "../../notification/snack.service";
 export class TopbarComponent {
 	public isMobileMenuOpen: boolean = false;
 	public menuArr: any = [
-		{
-			name: "Product",
-			description: "TV Haua",
-			link : "",
-			icon: `<svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-			viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-			<path stroke-linecap="round" stroke-linejoin="round"
-				d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
-			<path stroke-linecap="round" stroke-linejoin="round"
-				d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
-		</svg>`
-		},
-		{
-			name: "Store",
-			description: "TV Haua",
-			link : "",
-			icon: `<svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-			viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-			<path stroke-linecap="round" stroke-linejoin="round"
-				d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
-			<path stroke-linecap="round" stroke-linejoin="round"
-				d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
-		</svg>`
-		},
-		{
-			name: "About",
-			description: "AC Haua",
-			link : "",
-			icon: `<svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-			viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-			<path stroke-linecap="round" stroke-linejoin="round"
-				d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
-			<path stroke-linecap="round" stroke-linejoin="round"
-				d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
-		</svg>`
-		},
+		// {
+		// 	name: "Product",
+		// 	description: "TV Haua",
+		// 	link : "",
+		// 	icon: `<svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
+		// 	viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+		// 	<path stroke-linecap="round" stroke-linejoin="round"
+		// 		d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
+		// 	<path stroke-linecap="round" stroke-linejoin="round"
+		// 		d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
+		// </svg>`
+		// },
+		// {
+		// 	name: "Store",
+		// 	description: "TV Haua",
+		// 	link : "",
+		// 	icon: `<svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
+		// 	viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+		// 	<path stroke-linecap="round" stroke-linejoin="round"
+		// 		d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
+		// 	<path stroke-linecap="round" stroke-linejoin="round"
+		// 		d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
+		// </svg>`
+		// },
+		// {
+		// 	name: "About",
+		// 	description: "AC Haua",
+		// 	link : "",
+		// 	icon: `<svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
+		// 	viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+		// 	<path stroke-linecap="round" stroke-linejoin="round"
+		// 		d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
+		// 	<path stroke-linecap="round" stroke-linejoin="round"
+		// 		d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
+		// </svg>`
+		// },
 		{
 			name: "Contact Us",
 			description: "AC Haua",
