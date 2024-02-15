61622742952b2a98e360e34580a4856eac2519cb
Subhankar Saha <subhankarsaha2410@gmail.com>
2024-02-16 01:24:07 +0530

    add home page

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -3,42 +3,65 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   constructor() {
-    console.log("welcome")
+    console.log('welcome');
   }
   public imageBannerArr: any = [
-		{
-			name: "Image1",
-			description: "AC Haua",
-			src : "../../../assets/banner/image2fan.jpg",
-		},
-		{
-			name: "Image1",
-			description: "AC Haua",
-			src : "../../../assets/banner/image1fan.png",
-		},
     {
-			name: "Image1",
-			description: "AC Haua",
-			src : "../../../assets/banner/image2fan.jpg",
-		},
-		{
-			name: "Image1",
-			description: "AC Haua",
-			src : "../../../assets/banner/image1fan.png",
-		},
+      name: 'Image1',
+      description: 'AC Haua',
+      src: '../../../assets/banner/image2fan.jpg',
+    },
     {
-			name: "Image1",
-			description: "AC Haua",
-			src : "../../../assets/banner/image2fan.jpg",
-		},
-		{
-			name: "Image1",
-			description: "AC Haua",
-			src : "../../../assets/banner/image1fan.png"
-		},
-	]
+      name: 'Image1',
+      description: 'AC Haua',
+      src: '../../../assets/banner/image1fan.png',
+    },
+    {
+      name: 'Image1',
+      description: 'AC Haua',
+      src: '../../../assets/banner/image2fan.jpg',
+    },
+    {
+      name: 'Image1',
+      description: 'AC Haua',
+      src: '../../../assets/banner/image1fan.png',
+    },
+    {
+      name: 'Image1',
+      description: 'AC Haua',
+      src: '../../../assets/banner/image2fan.jpg',
+    },
+    {
+      name: 'Image1',
+      description: 'AC Haua',
+      src: '../../../assets/banner/image1fan.png',
+    },
+  ];
+
+  public productsArr: any = [
+    {
+      name: 'Fans',
+      description: 'AC Haua',
+      src: '../../../assets/images/ceiling-fan.png',
+    },
+    {
+      name: 'TVs',
+      description: 'AC Haua',
+      src: '../../../assets/images/television.png',
+    },
+    {
+      name: 'Home Theaters',
+      description: 'AC Haua',
+      src: '../../../assets/images/home-theater.png',
+    },
+	{
+		name: 'AC',
+		description: 'AC Haua',
+		src: '../../../assets/images/air-conditioner.png',
+	  }
+  ];
 }
