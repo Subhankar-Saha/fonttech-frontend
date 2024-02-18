07e5d2af35ba7c067be12942b268421ab52be477
Subhankar Saha <subhankarsaha2410@gmail.com>
2024-02-18 13:45:54 +0530

    add privacy and terms

--- src/app/navigation/footer/footer.component.ts
+++ src/app/navigation/footer/footer.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RequestMapperService } from 'src/app/request-mapper.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  constructor(private _router : Router){}
+
+  ngOnInit(): void {
+      
+  }
+
+  onClick(value:string){
+    if(value === 'PRIVACY'){
+      this._router.navigate([RequestMapperService.PRIVACY_URL])
+    }
+    else if(value === 'TERMS'){
+      this._router.navigate([RequestMapperService.TERM_URL])
+    }
+  }
+
 
 }
