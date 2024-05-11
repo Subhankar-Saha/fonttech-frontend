333fbbb3d2f4ba6c30a398709235e615756c70fe
Subhankar Saha <subhankarsaha2410@gmail.com>
2024-05-11 13:47:38 +0530

    add eagerloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./authentication/auth.guard";
 import { RequestMapperService } from "./request-mapper.service";
+import { HomeModule } from "./home/home.module";
+import { AuthenticationModule } from "./authentication/authentication.module";
+import { FranchiseModule } from "./franchise/franchise.module";
+import { ContactModule } from "./contact/contact.module";
+import { policyModule } from "./policy_document/policy.module";
+import { HomeComponent } from "./home/home/home.component";
 
 const routes: Routes = [
   {
@@ -13,38 +19,32 @@ const routes: Routes = [
   },
   {
     path: RequestMapperService.BASE_RELATIVE_URL,
-    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+    component: HomeComponent,
   },
   {
     path: RequestMapperService.HOME,
     // canActivate: [AuthGuard],
-    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+    component: HomeComponent,
   },
   {
     path: RequestMapperService.AUTH_URL,
     // canActivate: [AuthGuard],
-    loadChildren: () =>
-      import("./authentication/authentication.module").then(
-        (m) => m.AuthenticationModule,
-      ),
+    loadChildren: () => AuthenticationModule,
   },
   {
     path: RequestMapperService.FRANCHISE_URL,
     // canActivate: [AuthGuard],
-    loadChildren: () =>
-      import("./franchise/franchise.module").then((m) => m.FranchiseModule),
+    loadChildren: () => FranchiseModule,
   },
   {
     path: RequestMapperService.CONTACT_URL,
     // canActivate: [AuthGuard],
-    loadChildren: () =>
-      import("./contact/contact.module").then((m) => m.ContactModule),
+    loadChildren: () => ContactModule,
   },
   {
     path: RequestMapperService.PAGES_URL,
     // canActivate: [AuthGuard],
-    loadChildren: () =>
-      import("./policy_document/policy.module").then((m) => m.policyModule),
+    loadChildren: () => policyModule,
   },
 ];
 
